cdecfdb3f9778cc9c8d91bb72e4843c481ea7ff2
Phil Behnke <phil.behnke@gmail.com>
2011-07-27 00:24:26 -0400

    Test

---
+++ gui/gnu_radio/top_block.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Top Block
+# Generated: Mon Jul 25 18:51:01 2011
+##################################################
+
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import window
+from gnuradio.eng_option import eng_option
+from gnuradio.gr import firdes
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+class top_block(grc_wxgui.top_block_gui):
+
+	def __init__(self):
+		grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+
+		##################################################
+		# Variables
+		##################################################
+		self.sample_rate_each_stream = sample_rate_each_stream = 0
+		self.samp_rate = samp_rate = 5000000
+
+		##################################################
+		# Blocks
+		##################################################
+		self.gr_deinterleave_0 = gr.deinterleave(gr.sizeof_short*1)
+		self.gr_file_source_0 = gr.file_source(gr.sizeof_short*1, "/home/phil/fx2lib-git/fx2lib/examples/fpga_iso_2/multithread_c_app/my_fifo0123", True)
+		self.gr_float_to_complex_0 = gr.float_to_complex(1)
+		self.gr_short_to_float_0 = gr.short_to_float()
+		self.gr_short_to_float_1 = gr.short_to_float()
+		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, samp_rate)
+		self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+			self.GetWin(),
+			baseband_freq=20100000,
+			dynamic_range=100,
+			ref_level=50,
+			ref_scale=2.0,
+			sample_rate=samp_rate,
+			fft_size=512,
+			fft_rate=15,
+			average=False,
+			avg_alpha=None,
+			title="Waterfall Plot",
+		)
+		self.Add(self.wxgui_waterfallsink2_0.win)
+
+		##################################################
+		# Connections
+		##################################################
+		self.connect((self.gr_file_source_0, 0), (self.gr_deinterleave_0, 0))
+		self.connect((self.gr_deinterleave_0, 0), (self.gr_short_to_float_0, 0))
+		self.connect((self.gr_deinterleave_0, 1), (self.gr_short_to_float_1, 0))
+		self.connect((self.gr_short_to_float_1, 0), (self.gr_float_to_complex_0, 1))
+		self.connect((self.gr_short_to_float_0, 0), (self.gr_float_to_complex_0, 0))
+		self.connect((self.gr_float_to_complex_0, 0), (self.gr_throttle_0, 0))
+		self.connect((self.gr_throttle_0, 0), (self.wxgui_waterfallsink2_0, 0))
+
+	def set_sample_rate_each_stream(self, sample_rate_each_stream):
+		self.sample_rate_each_stream = sample_rate_each_stream
+
+	def set_samp_rate(self, samp_rate):
+		self.samp_rate = samp_rate
+		self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+
+if __name__ == '__main__':
+	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+	(options, args) = parser.parse_args()
+	tb = top_block()
+	tb.Run(True)
+
---
+++ usb_driver/dataGrabber.c
@@ -0,0 +1,351 @@
+#include <stdio.h>
+#include <libusb-1.0/libusb.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define NUM_PACKETS 32
+
+
+/* Continue Variable - Set to zero on SIGINT */
+int _continue = 1;
+
+/* Libusb context and device */
+libusb_context *mycontext;
+libusb_device_handle *my_dev_handle; 
+
+/* File point to the file where we store the data */
+static FILE *fp;
+
+char filename[50];
+int live_mode=0;
+
+/* This is the string sent every 61 bytes by the FPGA.  Used to verify the data was sent correctly */
+const unsigned char EOP[] = {'N','Y','A','N'};
+
+/* Constant string of 0x00.  We only get a lot of zeros when we don't send enough data */
+const unsigned char ZEROS[] = {0x0,0x0,0x0,0x0,0x0,0x0};
+
+/* This is used to make counting how much the data we get is valid */
+//int buffer_mark=0;
+
+/* Timing variables for testing */
+struct timeval t1, t2;
+double elapsedTime;
+
+unsigned char * fill_buffer1;
+unsigned char * fill_buffer2;
+unsigned char * fill_buffer3;
+unsigned char * fill_buffer4;
+unsigned char * fill_buffer5;
+unsigned char * fill_buffer6;
+unsigned char * fill_buffer7;
+unsigned char * fill_buffer8;
+
+
+/**********************************************************************************************
+* This function scans the data to look for the next packet (denoted by the NYAN string) in the set
+**********************************************************************************************/
+int getNextPacket(int currentIndex, struct libusb_transfer *transfer){
+	int i;
+
+	for(i=currentIndex;i<(3072*NUM_PACKETS)-4;i++){
+		if(!memcmp(&(transfer->buffer[i]),EOP,4)){
+			break;
+		}
+		//if we see a bunch of zeros, skip ahead
+		else if(!memcmp(&(transfer->buffer[i]),ZEROS,6)){
+			i=i+1024;
+		}
+	}
+	return i;
+
+}
+
+/**********************************************************************************************
+* Our Callback Function
+**********************************************************************************************/
+void callback_fn(struct libusb_transfer *transfer){
+
+	int i=0;
+	int j;
+	unsigned char tmp[64];
+	unsigned char i_1,i_2,j_1,j_2; //dont confuse these with our counter vars.
+
+	gettimeofday(&t1, NULL);
+
+	//find position of the first packet marker
+	i=getNextPacket(i,transfer);	
+
+	//process all the data and determine which parts are valid
+	do{
+		i=i+68;
+		if(!memcmp(&(transfer->buffer[i]),EOP,4)){
+
+			memcpy(tmp,&(transfer->buffer[i-64]),64);
+
+			//reshuffle the data for the i and j components
+			for(j=0;j<64;j+=4){
+				i_1=tmp[j];
+				i_2=tmp[j+2];
+				j_1=tmp[j+1];
+				j_2=tmp[j+3];
+
+				tmp[j+1]=i_2;
+				tmp[j+2]=j_1;
+				tmp[j+3]=j_2;
+			}
+			fwrite(tmp,(size_t)1,(size_t)64,fp);
+
+		}
+		else{
+			i=getNextPacket(i,transfer);
+		}	
+	}while(i<(3072*NUM_PACKETS)-1);
+
+	fflush(fp);
+
+	gettimeofday(&t2, NULL);  
+
+	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+	printf("%f\n",elapsedTime);
+
+	if(_continue){
+		libusb_submit_transfer(transfer);
+	}
+	else{
+		libusb_free_transfer(transfer);
+	}
+}	
+
+/**********************************************************************************************
+* Thread definition to create new libusb transfers
+**********************************************************************************************/
+void transfer_factory(void *param){
+	//create a packet
+
+	struct libusb_transfer *iso_xfer1;
+	struct libusb_transfer *iso_xfer2;
+	struct libusb_transfer *iso_xfer3;
+	struct libusb_transfer *iso_xfer4;
+	struct libusb_transfer *iso_xfer5;
+	struct libusb_transfer *iso_xfer6;
+	struct libusb_transfer *iso_xfer7;
+	struct libusb_transfer *iso_xfer8;
+
+	fill_buffer1=malloc(3072*NUM_PACKETS);
+	fill_buffer2=malloc(3072*NUM_PACKETS);
+	fill_buffer3=malloc(3072*NUM_PACKETS);
+	fill_buffer4=malloc(3072*NUM_PACKETS);
+	fill_buffer5=malloc(3072*NUM_PACKETS);
+	fill_buffer6=malloc(3072*NUM_PACKETS);
+	fill_buffer7=malloc(3072*NUM_PACKETS);
+	fill_buffer8=malloc(3072*NUM_PACKETS);
+
+	//create a new packet
+	iso_xfer1=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer1,my_dev_handle,0x82,fill_buffer1,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer1,3072);
+
+	iso_xfer2=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer2,my_dev_handle,0x82,fill_buffer2,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer2,3072);
+
+	iso_xfer3=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer3,my_dev_handle,0x82,fill_buffer3,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer3,3072);
+
+	iso_xfer4=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer4,my_dev_handle,0x82,fill_buffer4,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer4,3072);
+
+	iso_xfer5=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer5,my_dev_handle,0x82,fill_buffer5,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer5,3072);
+
+	iso_xfer6=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer6,my_dev_handle,0x82,fill_buffer6,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer6,3072);
+
+	iso_xfer7=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer7,my_dev_handle,0x82,fill_buffer7,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer7,3072);
+
+	iso_xfer8=libusb_alloc_transfer(NUM_PACKETS);
+	libusb_fill_iso_transfer(iso_xfer8,my_dev_handle,0x82,fill_buffer8,3072*NUM_PACKETS,NUM_PACKETS,callback_fn, NULL, 0);
+	libusb_set_iso_packet_lengths(iso_xfer8,3072);
+
+	libusb_submit_transfer(iso_xfer1); 
+	libusb_submit_transfer(iso_xfer2); 
+	libusb_submit_transfer(iso_xfer3); 
+	libusb_submit_transfer(iso_xfer4); 
+	libusb_submit_transfer(iso_xfer5); 
+	libusb_submit_transfer(iso_xfer6); 
+	libusb_submit_transfer(iso_xfer7); 
+	libusb_submit_transfer(iso_xfer8); 
+		
+	//magic number to sleep to prevent excessive submitted transfers
+	//usleep(3600);
+
+
+}
+
+
+
+/**********************************************************************************************
+* Signal Handler
+**********************************************************************************************/
+void main_signal_handler(int signum)
+{
+    switch (signum) {
+    case SIGINT:
+        _continue=0;
+	//pthread_join(factory,NULL);
+	//pthread_join(storage,NULL);
+
+        break;
+
+    }
+
+}
+
+
+void init(int argc, char *argv[])
+{
+    int opt;
+    extern char *optarg;
+    extern int optind, opterr, optopt;
+
+    while ((opt = getopt(argc, argv, "f:l::")) != -1) {
+        switch (opt) {
+        case 'f':
+            strcpy(filename,optarg);
+            break;
+        case 'l':
+	    live_mode=1;
+            //live_mode = atoi(optarg);
+            break;
+        default:
+            fprintf(stderr,
+                    "Usage: %s -f filename [-l]\n",
+                    argv[0]);
+            exit(-1);
+        }
+    }
+
+}
+
+/**********************************************************************************************
+* Main Function
+**********************************************************************************************/
+int main(int argc, char *argv[]){
+
+	int ret;
+
+	//parse command line options
+	init(argc, argv);
+	if(!strcmp(filename,"")){
+		fprintf(stderr,
+                    "Usage: %s -f filename [-l]\n",
+                    argv[0]);
+            exit(-1);
+	}
+	else if(live_mode){
+		ret=mkfifo(filename,0777);
+		if(ret==0){
+			fprintf(stderr,"Error, unable to create named pipe.\n");
+			exit(-2);
+		}
+	}
+
+	//init libusb
+	printf("Initalizing library...");
+	libusb_init(&mycontext);
+	printf("Done.\n");
+	libusb_set_debug(mycontext,3);
+	
+	//open the file
+	fp=fopen(filename, "wb");
+
+	//open fx2
+	printf("Opening device with VID 0x04b4 and PID 0x0410...");
+	my_dev_handle=libusb_open_device_with_vid_pid(mycontext, 0x04b4,0x0410);
+	if(my_dev_handle==NULL){
+		printf("\nERROR: UNABLE TO OPEN DEVICE.\n");
+	}
+	else{
+		printf("Successful.\n");
+	}
+
+	printf("Claiming interface 0...");
+	ret=libusb_claim_interface(my_dev_handle,0);
+	if(ret==0){
+		printf("Successful.\n");
+	}
+	else{
+		printf("\nERROR: UNABLE TO CLAIM DEVICE.\n");
+	}
+	printf("Setting Alt Interface Setting to 3\n");
+	ret=libusb_set_interface_alt_setting (my_dev_handle, 0, 3);
+	if(ret==0){
+		printf("Successful.\n");
+	}
+	else{
+		printf("\nERROR: UNABLE TO SET ALT INTERFACE.\n");
+	}
+
+	//register a handler for sigint
+    	struct sigaction act;
+    	act.sa_handler = main_signal_handler;
+    	sigemptyset(&act.sa_mask);
+    	act.sa_flags = 0;
+    	sigaction(SIGINT, &act, NULL);
+
+	transfer_factory(NULL);
+
+	//continuously call handle_events()
+	while(_continue)
+		libusb_handle_events(mycontext);
+
+	//close the file
+	fclose(fp);
+
+	free(fill_buffer1);
+	free(fill_buffer2);
+	free(fill_buffer3);
+	free(fill_buffer4);
+	free(fill_buffer5);
+	free(fill_buffer6);
+	free(fill_buffer7);
+	free(fill_buffer8);
+	
+
+	printf("\nReleasing interface 0...");
+	ret=libusb_release_interface(my_dev_handle,0);
+	if(ret==0){
+		printf("Successful.\n");
+	}
+	else{
+		printf("\nERROR: UNABLE TO RELEASE DEVICE.\n");
+	}
+
+
+	printf("Closing device...");
+	libusb_close(my_dev_handle);
+	printf("Done.\n");
+
+	printf("Destroying library...");
+	libusb_exit(mycontext);
+	printf("Done.\n");
+
+	return 0;
+} 
